3ca20147f2c66bc2be2a22f5dc13d92ba3bbad4d
root <robert@arles.us>
2016-11-26 14:05:52 -0800

    fixed up the uwsgi environment (removed pyt2.7 uwsgi) and committed a copy of the init.d script

--- rabot.sh
+++ rabot.sh
@@ -5,7 +5,7 @@ mkvirtualenv --python=/usr/bin/python3 rabot
 workon rabot
 pip3 install -r requirements.txt
 # debug mode, runs using flask server
-python rabot.py
+# python rabot.py
 # production mode, runs using uwsgi server
-#uwsgi --http-socket 127.0.0.1:5000 --wsgi-file rabot.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:5001
+uwsgi --http-socket 127.0.0.1:5000 --wsgi-file rabot.py --callable app --processes 4 --threads 2 --stats 127.0.0.1:5001
 popd
